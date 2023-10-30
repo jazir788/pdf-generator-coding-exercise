c0ded994de540eb1b8775bc003c22418b23e5037
jazir788 <jazirsx5@gmail.com>
2023-10-30 11:30:05 +0000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from fpdf import FPDF
+import pandas as pd
+import glob
+import openpyxl
+from pathlib import Path
+
+filepaths = glob.glob("Files/*.txt")
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+
+for filepath in filepaths:
+    pdf.add_page()
+    filename = Path(filepath).stem
+    name = filename.title()
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=name, ln = 1)
+
+    with open(filepath, "r") as file:
+        content = file.read()
+
+    pdf.set_font(family="Times", size=12, )
+    pdf.multi_cell(w=0, h=6, txt=content)
+
+    pdf.output("output.pdf")
+
+
+
